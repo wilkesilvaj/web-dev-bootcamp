a67cbd4a69e0b82a4066bf498bf5b840b6270d44
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-01-22 10:48:59 -0800

    Created an array and used the 'push', 'pop', 'unshift' and 'shift' functions on it

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ if (random < 0.5)   {
 else    {
     console.log("Your number is higher than 1.0")
 }
+
+
+const wowClasses = ['Fairy','Rogue', 'Warrior', 'Demon Hunter', 'Hunter', 'Death Knight'];
+console.log(wowClasses);
+
+// Removes the first item from the array, which is 'Fairy' 
+wowClasses.shift(); 
+console.log(wowClasses);
+
+// Adds 'Druid' at the start of the array
+wowClasses.unshift('Druid');
+console.log(wowClasses);
+
+// Adds 'Battle Mage' to the end of the array
+wowClasses.push('Battle Mage');
+console.log(wowClasses);
+
+// Removes the last item from the array, which is 'Battle Mage'
+wowClasses.pop();
+console.log(wowClasses);
+
