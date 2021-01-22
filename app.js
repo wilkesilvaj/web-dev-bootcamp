28ec0b60a328ecf2a5561d1c5ed86629d71d271b
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-01-22 10:39:35 -0800

    Added a simple conditional clause to print a message based on a random number

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Sup!");
+
+let random = Math.random();
+if (random < 0.5)   {
+    console.log("Your number is less than 0.5!");
+}
+else    {
+    console.log("Your number is higher than 1.0")
+}
