b7caa16a322737d3738894c3e5f02bd2669d6472
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-01-28 10:38:32 -0800

    Created two arrow functions

--- app.js
+++ app.js
@@ -18,17 +18,28 @@ const movies = [
     
 
 ];
-
+// Iterating through the array using ForEach
 console.log ("******** Using 'ForEach' ********");
 movies.forEach(function(movie) {
     console.log(`${movie.title} - ${movie.score}/100`);
 });
 
+// Iterating through the array using For Of
 console.log ("******** Using 'For OF' ********");
 for (let movie of movies)   {
     console.log(`${movie.title} - ${movie.score}/100`);
 }
 
+//  Example of the map function which returns an array with the movie titles UPPERCASED
 movies.map(function(movie)  {
     return movie.title.toUpperCase();
 });
+
+//  First example of an arrow function which simulates the roll of a dice
+const rollDie = () =>   {
+    return Math.floor(Math.random() *6) +1;
+}
+// Second example of an arrow function, which squares a number
+const square = num =>   {
+    return num*num;
+}
