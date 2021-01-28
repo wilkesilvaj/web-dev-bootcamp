d70a5b11b5ccc17b1524d69235e833779268f75b
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-01-28 10:48:15 -0800

    Added a map function which makes use of an arrow function

--- app.js
+++ app.js
@@ -15,31 +15,42 @@ const movies = [
         title: "Nanatsu No Taizai",
         score: 50
     },
-    
+
 
 ];
 // Iterating through the array using ForEach
-console.log ("******** Using 'ForEach' ********");
-movies.forEach(function(movie) {
+console.log("******** Using 'ForEach' ********");
+movies.forEach(function (movie) {
     console.log(`${movie.title} - ${movie.score}/100`);
 });
 
 // Iterating through the array using For Of
-console.log ("******** Using 'For OF' ********");
-for (let movie of movies)   {
+console.log("******** Using 'For OF' ********");
+for (let movie of movies) {
     console.log(`${movie.title} - ${movie.score}/100`);
 }
 
 //  Example of the map function which returns an array with the movie titles UPPERCASED
-movies.map(function(movie)  {
+movies.map(function (movie) {
     return movie.title.toUpperCase();
 });
 
 //  First example of an arrow function which simulates the roll of a dice
-const rollDie = () =>   {
-    return Math.floor(Math.random() *6) +1;
+const rollDie = () => {
+    return Math.floor(Math.random() * 6) + 1;
 }
 // Second example of an arrow function, which squares a number
-const square = num =>   {
-    return num*num;
+const square = num => {
+    return num * num;
 }
+
+// The following arrow function does the same as the one above it. It is just "shorter"
+const shortSquare = num => num * num;
+
+
+console.log("******** Using Arrow Functions within the MAP method ********");
+const newMovies = movies.map(movie => {
+    return `${movie.title} - ${movie.score / 10}`;
+});
+
+console.log(newMovies);
