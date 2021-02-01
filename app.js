095296fbc84bb8438dba8f9c85ac6ac904eeb1f3
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-01 10:38:19 -0800

    Made a container for each pokemon and added a span to show the pokemon's number as well

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
 const container = document.querySelector('#container');
 
 for (i = 1 ; i<= 151; i++)  {
-    const pokemon = document.createElement('img');
-    pokemon.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
-    container.appendChild(pokemon);
+    // Creates the pokemon container, the image and the span
+    const pokemonContainer = document.createElement('div');    
+    const pokemonImage = document.createElement('img');
+    const pokemonNumber = document.createElement('span'); 
+    
+    // Adds the desired content to the elements
+    pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+    pokemonNumber.innerText = `#${i}`;
+
+    // Appends the image and span to the 'eachPokemon' container
+    pokemonContainer.appendChild(pokemonImage);
+    pokemonContainer.appendChild(pokemonNumber);
+
+    // Appends the pokemonContainer to the main container section
+    container.appendChild(pokemonContainer);
 }
