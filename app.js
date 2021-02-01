871d21a1ec7fcb3a0450fe2735ee87968b315b20
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-01 10:31:57 -0800

    Edited the quantity to match the original 151 pokemons

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 const container = document.querySelector('#container');
 
-for (i = 1 ; i<= 100; i++)  {
-    let pokemon = document.createElement('img');
+for (i = 1 ; i<= 151; i++)  {
+    const pokemon = document.createElement('img');
     pokemon.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
     container.appendChild(pokemon);
 }
