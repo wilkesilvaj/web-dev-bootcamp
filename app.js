86c36bd50f120a84989ee89f27ada29bc50a6c1d
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-03 10:47:23 -0800

    Added another button which randomizes the background color upon being clicked.

--- app.js
+++ app.js
@@ -15,3 +15,16 @@ const btn3 = document.querySelector('#v3');
 btn3.addEventListener('click', () =>    {
     alert("You have clicked on button #3!")
 })
+
+// Code to randomize bgcolor
+const colorBtn = document.querySelector('#color');
+
+const randomizeColor = () =>    {
+    let r = Math.floor(Math.random()*255 + 1);
+    let g = Math.floor(Math.random()*255 + 1);
+    let b = Math.floor(Math.random()*255 + 1);
+    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
+
+
+colorBtn.addEventListener('click', randomizeColor);
