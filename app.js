89381ef2ca43d3d31906191420193595b9c82d15
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-09 09:43:28 -0800

    Created a fake 'login' async function to demonstrate the usage of async functions

--- app.js
+++ app.js
@@ -11,8 +11,8 @@
 // • 'throw' something, regardless if its an error, a String, etc
 
 // This is also valid syntax
-const sing = async ()   =>  {
-    if (Math.random() <= 0.7)   {
+const sing = async () => {
+    if (Math.random() <= 0.7) {
         return "Do Re Mi Fa Sol La Si Do";
     }
     throw "You cannot sing!";
@@ -20,10 +20,42 @@ const sing = async ()   =>  {
 }
 
 sing()
-    .then(data =>   {
-        console.log("Promise resolved with ",data);
+    .then(data => {
+        console.log("Promise resolved with ", data);
     })
-    .catch(err =>   {
-        console.log("Promise failed, ",err);
+    .catch(err => {
+        console.log("Promise failed, ", err);
     })
+
+
+const login = async (username, password) => {
+    if (!username || !password) throw 'Missing credentials';
+    if (password === '123456') return `Welcome, ${username}`;
+    throw 'Password error';
+
+    
+    
+}
+
+login('Johnny', '987654')
+    .then(msg => {
+        console.log("LOGIN SUCCESFUL!");
+        console.log(msg);
+    })
+    .catch(err => {
+        console.log("LOGIN FAILED!");
+        console.log(err)
+    })
+    
+login("Alina",'123456')
+    .then(msg => {
+        console.log("LOGIN SUCCESFUL!");
+        console.log(msg);
+    })
+    .catch(err => {
+        console.log("LOGIN FAILED!");
+        console.log(err)
+    })
+  
+    
 
