79a74c113aa63365f52e446867d93e4056a19505
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-12 09:41:01 -0800

    Parsed the actual data from the result object of the first 'fetch' function

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 fetch('https://api.cryptonator.com/api/ticker/btc-usd')
     .then(result => {
         console.log("RESPONSE ACQUIRED, WAITING FOR PARSE...");
-       
+        // The following method returns another PROMISE, for the actual data within the first FETCH to be parsed into JSON
+        return result.json();
+    })
+    .then(data =>   {
+        console.log("DATA SUCCESSFULLY PARSED!");
+        console.log(data.ticker.price);
     })
     .catch(e => {
         console.log('ERROR!');
