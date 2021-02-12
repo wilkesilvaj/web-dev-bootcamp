e025b2f59a6bd640d0ab554f8bc0d7d0a470fbc7
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-12 09:49:45 -0800

    Starter files

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-fetch('https://api.cryptonator.com/api/ticker/btc-usd')
-    .then(result => {
-        console.log("RESPONSE ACQUIRED, WAITING FOR PARSE...");
-        // The following method returns another PROMISE, for the actual data within the first FETCH to be parsed into JSON
-        return result.json();
-    })
-    .then(data => {
-        console.log("DATA SUCCESSFULLY PARSED!");
-        console.log(data.ticker.price);
-    })
-    .catch(e => {
-        console.log('ERROR!');
-    })
+// fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+//     .then(result => {
+//         console.log("RESPONSE ACQUIRED, WAITING FOR PARSE...");
+//         // The following method returns another PROMISE, for the actual data within the first FETCH to be parsed into JSON
+//         return result.json();
+//     })
+//     .then(data => {
+//         console.log("DATA SUCCESSFULLY PARSED!");
+//         console.log(data.ticker.price);
+//     })
+//     .catch(e => {
+//         console.log('ERROR!');
+//     })
 
 
-const fetchBitcoinPrice = async () => {
-    try {        
-        const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
-        const data = await res.json();
-        console.log(data.ticker.price);
-    }
-    catch (e) {
-        console.log("Something went wrong!", e)
-    }
-}
+// const fetchBitcoinPrice = async () => {
+//     try {        
+//         const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+//         const data = await res.json();
+//         console.log(data.ticker.price);
+//     }
+//     catch (e) {
+//         console.log("Something went wrong!", e)
+//     }
+// }
 
-fetchBitcoinPrice();
+// fetchBitcoinPrice();
