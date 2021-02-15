0bd114d9a80caec1f1fb4bd53f48e3fafa366449
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-15 13:27:30 -0800

    Added another function to return the 'hexadecimal' value for that color and created a constructor function for the 'Color' object, along with 2 prototypes functions for that object

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 function makeColor(r, g, b) {
     const color = {};
-color.r = r;
-color.g = g;
-color.b = b;
-color.rgb = function() {
-    
-    return `rgb(${this.r},${this.g},${this.b})`;
-}
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function () {
+
+        return `rgb(${this.r},${this.g},${this.b})`;
+    }
+    color.hex = function()  {
+        const {r,g,b} = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+
     return color;
 }
 
-const firstColor = makeColor(35,255,150);
+const firstColor = makeColor(35, 255, 150);
+
+
+// Constructor function
+function Color(r,g,b)   {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+}
+
+// Adds protoype functions to all 'Color' objects
+Color.prototype.rgb = function()    {
+    return `rgb(${this.r},${this.g},${this.b})`;
+}
+
+Color.prototype.hex = function()    {
+    const {r,g,b} = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+const color1 = new Color(40,50,60);
+const color2 = new Color(0,0,0);
