ce50a616373799e130ef0a4ad72b84827246f697
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-16 09:28:10 -0800

    Created 2 simple classes, 'cat' and 'dog'

---
+++ animals.js
@@ -0,0 +1,21 @@
+class Cat  {
+    constructor(name, age)   {
+    this.name = name;
+    this.age = age;
+    }
+
+    eat()   {
+        return `${this.name} is eating!`;
+    }
+}
+
+class Dog  {
+    constructor(name, age)   {
+    this.name = name;
+    this.age = age;
+    }
+
+    eat()   {
+        return `${this.name} is eating!`;
+    }
+}
