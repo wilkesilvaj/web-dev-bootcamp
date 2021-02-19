39f187bcd67e655a6f86a6141601893fe8677c83
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-19 09:40:50 -0800

    Required the necessary file and added its feature to color console text to the callback function printing 'dad jokes'

--- Jokester/index.js
+++ Jokester/index.js
@@ -1,5 +1,8 @@
 const jokes = require('give-me-a-joke');
-
+const colors = require('colors');
 jokes.getRandomDadJoke(function (joke)  {
-    console.log(joke);
-} )
+    console.log(joke.rainbow);
+
+});
+
+
