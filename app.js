e567047439a1bc87779c104f4357edd521336167
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-22 09:56:26 -0800

    Required the 'langs' package, to retrieve the full language's name based on the 3-digit code

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ const inputText = process.argv[2];
 
 // Gets the language code from the argument(s) provided
 let langCode = franc(inputText);
+
+// Prints the language 3-digit code
 console.log(langCode);
 
+// Prints the language's name in English
+console.log(langs.where('3', langCode).name);
+// Prints the language's local / original name
+console.log(langs.where('3', langCode).local);
