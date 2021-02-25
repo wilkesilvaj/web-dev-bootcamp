3945ee8b8c3bb6c8252b57c957029d3686d86911
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-25 09:40:15 -0800

    Add a conditional clause to only retrieve the 'subreddit' template if there's data about it. If not, it will retrieve a 'notfound' template

--- app.js
+++ app.js
@@ -10,34 +10,38 @@ const redditData = require('./data.json');
 app.set('view engine', 'ejs');
 
 // This gets the directory name which 'app.js' is stored, and adds '/views' for the views root directory
-app.set('views',path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/views'));
 
 // This is the response from the server for the 'home' in our localhost:3000
-app.get('/', (req, res) =>  {
+app.get('/', (req, res) => {
     res.render('home.ejs');
 });
 
 // This is the response for requests for 'localhost:3000/r/anythingHere'
-app.get('/r/:subreddit', (req, res) =>  {
-    const {subreddit} = req.params;
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
     const data = redditData[subreddit]
-    console.log(data);
-    res.render('subreddit', {...data});
+    if (data) {
+        res.render('subreddit', { ...data });
+    }
+    else    {
+        res.render('notfound', {subreddit});
+    }
 });
 
 // This is the response for requests for 'localhost:3000/cats'
-app.get('/cats', (req, res) =>  {
+app.get('/cats', (req, res) => {
     const cats = ['Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'];
-    res.render('cats', {cats});
+    res.render('cats', { cats });
 });
 
 
 // This is the response for requests for 'localhost:3000/rand'
-app.get('/rand', (req, res) =>  {
-    const num = Math.floor(Math.random() * 10) +1;
-    res.render('random.ejs', {num});
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random.ejs', { num });
 });
 
-app.listen(3000, () =>  {
+app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
