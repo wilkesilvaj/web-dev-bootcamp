7fb1ffc3594e495d01be52072cdf495945939522
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-25 08:52:52 -0800

    Created a template, or a 'view' within the new folder, and send the 'home.ejs' page as a response from all requests received by my server

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { ppid } = require('process');
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>  {
-    app.send('Hi!');
-})
+    res.render('home.ejs');
+});
 
 app.listen(3000, () =>  {
     console.log('Listening on port 3000');
