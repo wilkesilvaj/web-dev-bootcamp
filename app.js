90618733629a534b8674579309a16c7479c3e121
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-02-25 08:56:44 -0800

    Required 'path' and set the 'views' path to be the directory which 'app.js' is located, joined by '/views', rather than trying to used the current working directory + '/views'. This allows me to run this script from any folder

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+
+// This gets the directory name which 'app.js' is stored, and adds '/views' for the views root directory
+app.set('views',path.join(__dirname, '/views'));
+
 
 app.get('/', (req, res) =>  {
     res.render('home.ejs');
