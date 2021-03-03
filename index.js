d2166194e36b6f5efa27c0764fcae4ce73f6bbfb
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-03-03 08:58:52 -0800

    Added EJS to the list of dependencies, created a 'comments' objects to be used in the future, set the 'view engine' to my 'views' folder and required 'path' to be able to set the absolute path for the views used by my demo

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const app = express();
+const path = require("path");
+
+const comments = [
+    {
+        username: 'Todd', 
+        comment: 'lool that is very funny!'
+    },
+    {
+        username: 'Brock', 
+        comment: 'OMG that is craaaaaaaaaaaaazy!'
+    },
+    {
+        username: 'Laura', 
+        comment: 'Well, I do love to go skiing'
+    },
+    {
+        username: 'Bob', 
+        comment: 'Man, I sure miss not having these COVID restrinctions!'
+    },
+    {
+        username: 'Mandy', 
+        comment: 'Work has been so stressful lately!'
+    }
+]
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/tacos', (req, res) => {
     res.send('GET /tacos response!');
