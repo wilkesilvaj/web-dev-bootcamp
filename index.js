bc7016dccad75f7b3d32aff07af5309424fc7cf1
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-03-15 09:34:33 -0700

    Created a schema for my new 'movie' object

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/movies', {useNewUrlParser: true, useUnifiedTopology: true});
 
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connection Open!');
-});
+mongoose.connect('mongodb://localhost:27017/movies', {useNewUrlParser: true, useUnifiedTopology: true})
+.then( () =>    {
+    console.log('Connection Open!');
+})
+.catch(err =>   {
+    console.log('Oh NO!! Connection failed!!');
+})
+
+
+const movieSchema = new mongoose.Schema ({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+})
