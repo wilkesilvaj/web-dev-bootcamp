b1d33a128a05e5ebe71cca1627a62d2cbf4ab0ff
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-03-15 09:53:10 -0700

    Created a new movie and saved it to my collection via the console

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect('mongodb://localhost:27017/movies', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true})
 .then( () =>    {
     console.log('Connection Open!');
 })
@@ -9,10 +9,15 @@ mongoose.connect('mongodb://localhost:27017/movies', {useNewUrlParser: true, use
     console.log('Oh NO!! Connection failed!!');
 })
 
-
+// Movies Schema
 const movieSchema = new mongoose.Schema ({
     title: String,
     year: Number,
     score: Number,
     rating: String
-})
+});
+
+// Creates the object / model based on my previously defined schema
+const Movie = mongoose.model('Movie', movieSchema);
+
+const amadeus = new Movie({title: 'Amadeus',year: 1986,score: 9.2,rating: 'R'});
