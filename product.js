0cce919af694e6a3e449f2a7ccfc999636f1b3b7
Joao Vitor Wilke Silva <wilkesilvaj@student.douglascollege.ca>
2021-03-16 09:44:10 -0700

    Created a new schema, connected it to a model and created an example of the 'Product' model

--- product.js
+++ product.js
@@ -1,10 +1,33 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/shopApp', {useNewUrlParser: true, useUnifiedTopology: true})
 .then( () =>    {
     console.log('Connection Open!');
 })
 .catch(err =>   {
     console.log('Oh NO!! Connection failed!!');
 })
+
+// Creates a new Schema / Skeleton for our 'objects'
+const productSchema = new mongoose.Schema({
+    name:   {
+        type: String,
+        required: true
+    },
+    price:  {
+        type: Number        
+    }
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+const bike = new Product({name: 'Mountain Bike', price: 599});
+bike.save()
+    .then(data =>   {
+        console.log(`${data.name} was succesfully added to the DB!`);
+    })
+    .catch(err =>   {
+        console.log('Ooops! Something went wrong!');
+        console.log(err);
+    })
